1f5ec45e106de3de948b614f4eb7ff7bb3a872f8
Kevin Shreve <kshreve@gmail.com>
2017-11-01 10:48:25 -0400

    add redux-form

--- src/client/redux/index.js
+++ src/client/redux/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 
 import todo from './ducks/todo';
 
 export default combineReducers({
     todo,
+    form,
 });
